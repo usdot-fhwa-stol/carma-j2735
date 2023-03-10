3bf0dbed33031c37ec921c059bf6e0f647366b70
Rob Baily <rob.baily@leidos.com>
2023-03-10 14:01:47 -0500

    Update ASN to version as of 2023-02-21.  Update branch in CARMASensitive used to get SAE ASN.

--- generate.sh
+++ generate.sh
@@ -11,16 +11,45 @@ fi
 rm -rf output
 mkdir -p output
 
-STARTING_TEST_MESSAGE=00
-REPLACE_UP_TO_TEST_MESSAGE=07
+# break up the CARMA ASN file based on the sections
+CARMA_ASN_FILE=asn/carma_j2735.asn
+OUTPUT_FILE=output/carma_complete_j2735.asn
+awk '/START MAIN_REPLACEMENT/{ f = 1; next } /END MAIN_REPLACEMENT/{ f = 0 } f' ${CARMA_ASN_FILE} > output/main_replacement.txt
+awk '/START REMOVE/{ f = 1; next } /END REMOVE/{ f = 0 } f' ${CARMA_ASN_FILE} > output/remove.txt
+awk '/START APPEND/{ f = 1; next } /END APPEND/{ f = 0 } f' ${CARMA_ASN_FILE} > output/append.txt
+awk '/START REGION_ADD/{ f = 1; next } /END REGION_ADD/{ f = 0 } f' ${CARMA_ASN_FILE} > output/region_add.txt
+awk '/START REGION_DEFINITIONS/{ f = 1; next } /END REGION_DEFINITIONS/{ f = 0 } f' ${CARMA_ASN_FILE} > output/region_definitions.txt
 
-STARTING_REPLACEMENT_LINE=$(grep -n "TestMessage${STARTING_TEST_MESSAGE} ::=" asn/sae/*.asn | cut -d: -f1)
-REPLACE_UP_TO_REPLACEMENT_LINE=$(grep -n "TestMessage${REPLACE_UP_TO_TEST_MESSAGE} ::=" asn/sae/*.asn | cut -d: -f1)
+# create the output file to start
+cat asn/sae/*.asn > ${OUTPUT_FILE}
+
+# do the removal
+grep -Fvf output/remove.txt ${OUTPUT_FILE} > ${OUTPUT_FILE}.tmp
+mv ${OUTPUT_FILE}.tmp ${OUTPUT_FILE}
+
+
+# add in the region definitions
+sed -i "/REGION DEFINITIONS AUTOMATIC TAGS::= BEGIN/ r output/region_definitions.txt" ${OUTPUT_FILE}
+
+# do the main replacement of the test messages
+STARTING_TEST_MESSAGE=00
+REPLACE_UP_TO_TEST_MESSAGE=08
+
+STARTING_REPLACEMENT_LINE=$(grep -n "TestMessage${STARTING_TEST_MESSAGE} ::=" ${OUTPUT_FILE} | cut -d: -f1)
+REPLACE_UP_TO_REPLACEMENT_LINE=$(grep -n "TestMessage${REPLACE_UP_TO_TEST_MESSAGE} ::=" ${OUTPUT_FILE} | cut -d: -f1)
 REPLACE_UP_TO_REPLACEMENT_LINE=$(expr ${REPLACE_UP_TO_REPLACEMENT_LINE} - 1)
 INSERTION_REPLACEMENT_LINE=$(expr ${STARTING_REPLACEMENT_LINE} - 1)
 
 echo "Replacing lines ${STARTING_REPLACEMENT_LINE} to ${REPLACE_UP_TO_REPLACEMENT_LINE} in SAE J2735"
-sed "${STARTING_REPLACEMENT_LINE},${REPLACE_UP_TO_REPLACEMENT_LINE}d" asn/sae/*.asn | sed -e "${INSERTION_REPLACEMENT_LINE}r asn/carma_j2735.asn" > output/carma_complete_j2735.asn
+sed "${STARTING_REPLACEMENT_LINE},${REPLACE_UP_TO_REPLACEMENT_LINE}d" ${OUTPUT_FILE} | sed -e "${INSERTION_REPLACEMENT_LINE}r output/main_replacement.txt" > ${OUTPUT_FILE}.tmp
+mv ${OUTPUT_FILE}.tmp ${OUTPUT_FILE}
+
+# do the appending, add newline to end of file just in case
+echo >> ${OUTPUT_FILE}
+cat output/append.txt >> ${OUTPUT_FILE}
+
+# add in the region id
+sed -i "/-- The values 128 and above are for local region use/ r output/region_add.txt" ${OUTPUT_FILE}
 
 pushd output
 
